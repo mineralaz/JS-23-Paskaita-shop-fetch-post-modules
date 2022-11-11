587fd42418378f133d58db5138b7746a0b3a6120
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-11 09:03:09 +0200

    one item

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 "use strict";
 console.log("main.js");
 
-class Shop {}
+class Shop {
+  // html element targets
+  el = {};
+  items = [];
+
+  constructor() {
+    this.initTargets();
+    this.getItems();
+  }
+
+  initTargets() {
+    this.el.list = document.getElementById("products");
+  }
+  getItems() {
+    getProducts().then((products) => {
+      this.items = products;
+    });
+  }
+}
+
+const app = new Shop();
+console.log("app ===", app);
