91d67eb430cc065dddfe80856fd9f8a45456d8b6
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-11 09:30:21 +0200

    renderList

--- js/main.js
+++ js/main.js
@@ -17,9 +17,34 @@ class Shop {
   getItems() {
     getProducts().then((products) => {
       this.items = products;
+      this.renderList();
     });
   }
-}
+
+  makeOneItem(itemObj) {
+    const divEl = document.createElement("div");
+    divEl.className = "shop-item card";
+    divEl.innerHTML = `
+        <img src="${itemObj.thumbnail}" alt="preke">
+        <h3>${itemObj.title}</h3>
+        <p class="price">${itemObj.price} eur</p>
+        <p>Category: ${itemObj.category}</p>
+        <div class="control">
+            <button>Add to cart</button>
+            <a href="#">more info ></a>
+        </div>
+
+`;
+    return divEl;
+  }
+
+  renderList() {
+    this.el.list.innerHTML = "";
+    this.items
+      .map((iObj) => this.makeOneItem(iObj))
+      .forEach((htmlEl) => this.el.list.append(htmlEl));
+  }
+} // class end
 
 const app = new Shop();
 console.log("app ===", app);
